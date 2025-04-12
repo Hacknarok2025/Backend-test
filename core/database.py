44d9ce61a88218cb8afdf9ea3058615da3230977
mlorenc <142215274+loureq177@users.noreply.github.com>
2025-04-12 13:24:00 +0200

    redefined the database

--- core/database.py
+++ core/database.py
@@ -1,23 +1,20 @@
 from typing import Annotated
 from fastapi import Depends
 from sqlmodel import Field, Session, SQLModel, create_engine
-from datetime import date, time
 from dotenv import load_dotenv
 import os
 
 
-class EventBase(SQLModel):
+class UserBase(SQLModel):
 
-    title: str
-    description: str | None
-    date: date
-    start_time: time
-    end_time: time
+    name : str = Field(unique=True)
+    email: str | None = Field(unique=True)
+    score: int = Field(default=0, ge=0, le=100) # zakres 0-100
+    current_level: int = Field(default=1, ge=1, le=10) # minimum level 1, maks level 10
 
+class User(UserBase, table=True):
 
-class Event(EventBase, table=True):
-
-    __tablename__ = "events"
+    __tablename__ = "users"
     model_config = {"arbitrary_types_allowed": True}
 
     id: int = Field(primary_key=True)
---
+++ pomysl.md
@@ -0,0 +1,27 @@
+Temat hackathonu "Hacknarok":
+# "Połączyć 9 światów w jeden kod"
+
+
+1. Strona główna:
+- Logo z funkcją przejścia do HOME
+- Opis
+- Przycisk "Start"
+- 
+
+### Pytania kontrolne
+
+1. Czy ten pomysł wpasowuje się świat wikingów?
+2. Czy ten pomysł korzysta z 9 światów?
+2. Czy wpasowuje się w social media?
+
+
+### Światy w mitologii nordyckiej
+- Asgard - świat Asów (bogów)
+- Alfheim - świat Jasnych Elfów
+- Nidavellir (lub Svartalfheim) - świat Krasnoludów/Ciemnych Elfów
+- Midgard - świat ludzi
+- Jotunheim - świat Olbrzymów
+- Vanaheim - świat Vanów (drugi ród bogów)
+- Niflheim - świat lodu i mgły
+- Muspelheim - świat ognia
+- Helheim - świat umarłych
