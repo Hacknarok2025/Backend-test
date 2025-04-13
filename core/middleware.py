4260d5cd3fec7750e367bce54bf8812b919d2873
Przemysław Zieliński <zprzemek378@gmail.com>
2025-04-13 06:46:01 +0200

    cors

--- core/middleware.py
+++ core/middleware.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",
+        "http://localhost:5174",  
+    "http://127.0.0.1:5174",
+    "https://hacknarok2025.github.io"
+
+]
+
 
 def add_cors_middleware(app: FastAPI):
     app.add_middleware(
